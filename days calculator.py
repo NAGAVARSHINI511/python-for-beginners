914605b967f3adc467777144c04c515014e4656a
Nagavarshini <109608418+NAGAVARSHINI511@users.noreply.github.com>
2022-10-14 20:59:31 +0530

    Add files via upload
    this calculates the day ,weeks and months that u have left to live

---
+++ days calculator.py
@@ -0,0 +1,7 @@
+age = int(input("What is your current age? "))
+age_end=int(input("what is the age you want to live till? "))
+age_left=int(age_end-age)
+d=int(age_left*365)
+w=int(age_left*52)
+m=int(age_left*12)
+print(f"You have {d} days, {w} weeks, and {m} months left.")

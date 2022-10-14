f187939c8fd2746c3dd097e9f516233f142212e8
Nagavarshini <109608418+NAGAVARSHINI511@users.noreply.github.com>
2022-10-14 20:53:09 +0530

    Add files via upload
    this calculates bill on according tip u want to give (10 12 or 15)
    and splits the bill for each person

---
+++ tip calculator.py
@@ -0,0 +1,11 @@
+
+bill = float(input("What was the total bill?"))
+tip = int(input("How much tip would you like to give? 10, 12, or 15?"))
+people = int(input("How many people to split the bill?"))
+tip_p = tip / 100
+total_tip = tip_p * bill
+bill_t = bill + total_tip
+split = bill_t / people
+final_amt = round(split, 2)
+final_amt=round(split,2)
+print(f"each person should pay {final_amt}")

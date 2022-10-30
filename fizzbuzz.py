c324a9c31e734a294ee5f10a2ed77dba523cc783
Nagavarshini <109608418+NAGAVARSHINI511@users.noreply.github.com>
2022-10-30 19:45:44 +0530

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,9 @@
 for n in range(1,101):
     if(n%3==0 and n%5==0):
      print("FizzBuzz")
-    elif(n%3==0):
+    elif(n%3==0): #if the num is divisible by 3 reminder is 0
      print("Fizz")
-    elif(n%5==0):
+    elif(n%5==0): #if the num is divisible by 5 reminder is 0
      print("Buzz")
     else:
      print(n)

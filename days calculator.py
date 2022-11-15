24fab02d8d1b82a2c2fd7ced91ebd6e3907fa2b4
Nagavarshini <109608418+NAGAVARSHINI511@users.noreply.github.com>
2022-11-15 22:17:12 +0530

    Update days calculator.py

--- days calculator.py
+++ days calculator.py
@@ -4,4 +4,4 @@ age_left=int(age_end-age)
 d=int(age_left*365)
 w=int(age_left*52)
 m=int(age_left*12)
-print(f"You have {d} days, {w} weeks, and {m} months left.")
+print(f"You have {d} days, {w} weeks, and {m} months left.")  #use {} inside print function to print value of variable

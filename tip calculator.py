4a89ebb61c8da367f807f7132c5e64ed31b2c498
Nagavarshini <109608418+NAGAVARSHINI511@users.noreply.github.com>
2022-11-29 22:31:45 +0530

    Update tip calculator.py

--- tip calculator.py
+++ tip calculator.py
@@ -1,4 +1,4 @@
-
+#program to find the tip
 bill = float(input("What was the total bill?"))
 tip = int(input("How much tip would you like to give? 10, 12, or 15?"))
 people = int(input("How many people to split the bill?"))

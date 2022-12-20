bfc4c1449a68fa66b0bd32c3dd2dcf6c73990fd4
Nagavarshini <109608418+NAGAVARSHINI511@users.noreply.github.com>
2022-12-21 00:08:13 +0530

    Update BMI 2.py

--- BMI 2.py
+++ BMI 2.py
@@ -4,7 +4,7 @@ weight = float(input("enter your weight in kg: "))
 
 bmi=round(weight/(height**2))
 if bmi<=18.5:
-    print(f"Your BMI is {bmi}, you are under weight. ") //bmi iss body mass index of a person
+    print(f"Your BMI is {bmi}, you are under weight. ") #bmi iss body mass index of a person
 elif bmi<=25:
     print(f"Your BMI is {bmi}, you have a normal weight.")
 elif bmi<=30:
